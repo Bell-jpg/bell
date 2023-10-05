cdcad90d48e553e6c8a4a172d99ff53b94a3b39c
Bell-jpg <59510034+Bell-jpg@users.noreply.github.com>
2023-10-05 13:02:38 +0300

    Create Task.py
    Task class

---
+++ Task.py
@@ -0,0 +1,16 @@
+class Task:
+    def __init__(self, name, description, due_date, completed):
+        self.name = name
+        self.description = description
+        self.due_date = due_date
+        self.completed = False
+
+
+
+def is_complete(self):
+    self.completed = True
+
+
+def check_if_complete(self):
+    return self.completed
+
